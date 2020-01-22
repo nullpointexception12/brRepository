e820d4c04ad1d4c2fb894ff10d95ef190896bd07
nullpointexception12 <576554202@qq.com>
2020-01-22 17:32:59 +0800

    1

---
+++ 01-brhkq-enterprise-crm/src/main/java/com/brhkq/enterprise/BrhkqEnterpriseCrmApplication.java
@@ -0,0 +1,13 @@
+package com.brhkq.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrhkqEnterpriseCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrhkqEnterpriseCrmApplication.class, args);
+	}
+
+}
